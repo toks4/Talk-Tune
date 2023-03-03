51b34c311e7e3f7ac6fc54ec5c6a8a36fe191ad5
Toks4shima <toks4shima@gmail>
2023-03-03 13:12:39 +0100

    test

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Route } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+   <Route>
     <App />
+    </Route>
   </React.StrictMode>,
 )
