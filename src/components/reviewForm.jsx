4e9f468d97680e1b19afc701bc4338a638e2759a
Matan Post <matanpost@gmail.com>
2023-03-08 13:21:44 +0100

    reviews start

--- src/components/reviewForm.jsx
+++ src/components/reviewForm.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import { useState } from 'react'
 
 function reviewForm() {
+  const [rating, setRating] = useState(0);
+  const [content, setContent] = useState('')
     
+  handleReviewSubmit = async (event) => {
+    event.preventDefault()
+  }
+
   return (
-    <form >
-        <label>Rating:<input type='number' value={rating} onChange={}/></label>
-        <label>Review:<input type='text' value={review} onChange={}/></label>
+    <form onSubmit={handleReviewSubmit}>
+        <label>Rating:<input type='number' value={rating} onChange={event => setRating(event.target.value)}/></label>
+        <label>Review:<input type='text' value={content} onChange={event => setContent(event.target.value)}/></label>
         <button type='submit'>Post</button>
     </form>
   )
